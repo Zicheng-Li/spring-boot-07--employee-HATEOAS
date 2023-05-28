217817f6df3da9173ee865264eb84490c3b53243
ZL&EL <lzc123456ab@gmail.com>
2023-05-27 23:58:36 -0400

    1

--- src/main/java/com/example/lzc/cruddemo/dao/EmployeeRepository.java
+++ src/main/java/com/example/lzc/cruddemo/dao/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.example.lzc.cruddemo.dao;
 import com.example.lzc.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+//@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     // that is it, no need to write anything
 }
